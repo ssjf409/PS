149dc9f973286efc59f4453d26df46923e538c81
DongHyeokJang <ssjf409@naver.com>
2020-04-17 01:19:17 +0900

    200417

--- 분할정복/10830행렬제곱02.cpp
+++ 분할정복/10830행렬제곱02.cpp
@@ -20,11 +20,71 @@ public:
         }
         delete[] arr;
     }
+    SquareMatrix(SquareMatrix& sm) : m_size(sm.m_size) {
+        arr = new int* [m_size];
+        for(int i = 0; i < m_size; i++) {
+            arr[i] = new int[m_size];
+        }
+        for(int i = 0; i < m_size; i++) {
+            for(int j = 0; j < m_size; j++) {
+                arr[i][j] = sm.arr[i][j];
+            }
+        }
+    }
 
-    
+    SquareMatrix& operator*(SquareMatrix& b) {
+        SquareMatrix result(m_size);
+        for(int i = 0; i < m_size; i++) {
+            for(int j = 0; j < m_size; j++) {
+                result.arr[i][j] = 0;
+            }
+        }
+        for(int i = 0; i < m_size; i++) {
+            for(int j = 0; j < m_size; j++) {
+                for(int k = 0; k < m_size; k++) {
+                    result.arr[i][j] += (arr[i][k] * b.arr[k][j]);
+                }
+            }
+        }
+
+        for(int i = 0; i < m_size; i++) {
+            for(int j = 0; j < m_size; j++) {
+                arr[i][j] = result.arr[i][j];
+            }
+        }
+        return *this;
+    }
+
+    int* operator[](int i) {
+        return arr[i];
+    }
+
+    friend istream& operator>>(istream &is, SquareMatrix &sm);
+    friend ostream& operator<<(ostream &is, SquareMatrix &sm);
 };
 
 
+istream &operator>>(istream &is, SquareMatrix &sm)
+{
+        for (int i = 0; i<sm.m_size; i++)
+               for (int j = 0; j < sm.m_size; j++)
+               {
+                       cout << i << "행 " << j << "열 입력: ";
+                       is >> sm[i][j];
+               }
+        return is;
+}
+
+ostream &operator<<(ostream &os, SquareMatrix &sm)
+{
+        for (int i = 0; i < sm.m_size; i++)
+        {
+               for (int j = 0; j < sm.m_size; j++)
+                       os << sm[i][j] << " ";
+               os << endl;
+        }
+        return os;
+}
 int main() {
 
     return 0;
