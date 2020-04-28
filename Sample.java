9edd0ecb729b9b4c43a4d114c7ae857298f19935
JangDonghyeok <ssjf409@naver.com>
2020-04-29 01:36:43 +0900

    200429

---
+++ Sample.java
@@ -0,0 +1,9 @@
+
+public class Sample {
+    public static void main(String args[]) {
+        int x = 10;
+        System.out.println("Hello World");
+
+    }
+
+}
--- 연습장.cpp
+++ 연습장.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <algorithm>
-#include <string> 
 
 using namespace std;
 
+
 int main() {
-    string str = "Hello World!";
-    transform(str.begin(), str.end(), str.begin(), ::tolower);
-    cout << str;
+    
     return 0;
 }
